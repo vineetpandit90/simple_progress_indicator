9ca089e76ce0b006a8bad286e52a9b5fceb11912
vineetsharma <vineet.sharma@girnarsoft.com>
2019-11-25 18:56:41 +0530

    first commit

--- android/src/main/java/progressindicator/vineet/com/simple_progress_indicator/SimpleProgressIndicatorPlugin.java
+++ android/src/main/java/progressindicator/vineet/com/simple_progress_indicator/SimpleProgressIndicatorPlugin.java
@@ -1,25 +1,62 @@
 package progressindicator.vineet.com.simple_progress_indicator;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/** SimpleProgressIndicatorPlugin */
+/**
+ * SimpleProgressIndicatorPlugin
+ */
 public class SimpleProgressIndicatorPlugin implements MethodCallHandler {
-  /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "simple_progress_indicator");
-    channel.setMethodCallHandler(new SimpleProgressIndicatorPlugin());
-  }
+    /**
+     * Plugin registration.
+     */
+//  public static void registerWith(Registrar registrar) {
+//    final MethodChannel channel = new MethodChannel(registrar.messenger(), "simple_progress_indicator");
+//    channel.setMethodCallHandler(new SimpleProgressIndicatorPlugin());
+//  }
+    Activity activity;
+    MethodChannel methodChannel;
 
-  @Override
-  public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
+    SimpleProgressIndicatorPlugin(Activity activity, MethodChannel methodChannel) {
+        this.activity = activity;
+        this.methodChannel = methodChannel;
+        methodChannel.setMethodCallHandler(this);
     }
-  }
+
+    /**
+     * Plugin registration.
+     */
+    public static void registerWith(Registrar registrar) {
+        final MethodChannel channel = new MethodChannel(registrar.messenger(), "simple_progress_indicator");
+        SimpleProgressIndicatorPlugin progressIndicatorPlugin = new SimpleProgressIndicatorPlugin(registrar.activity(), channel);
+        channel.setMethodCallHandler(progressIndicatorPlugin);
+    }
+
+    @Override
+    public void onMethodCall(MethodCall call, Result result) {
+        ProgressDialog progressDialog = new ProgressDialog(activity, R.style.MyTheme);
+        progressDialog.setCancelable(true);
+        progressDialog.setCanceledOnTouchOutside(true);
+        progressDialog.setIndeterminate(true);
+
+
+        if (call.method.equals("showProgressIndicator")) {
+            boolean isShow = (boolean) call.arguments;
+            if (progressDialog != null) {
+                if (!progressDialog.isShowing() && isShow) {
+                    progressDialog.show();
+                } else {
+                    progressDialog.dismiss();
+                }
+            }
+        } else {
+            result.notImplemented();
+        }
+    }
 }
--- ios/Classes/SwiftSimpleProgressIndicatorPlugin.swift
+++ ios/Classes/SwiftSimpleProgressIndicatorPlugin.swift
@@ -6,9 +6,22 @@ public class SwiftSimpleProgressIndicatorPlugin: NSObject, FlutterPlugin {
     let channel = FlutterMethodChannel(name: "simple_progress_indicator", binaryMessenger: registrar.messenger())
     let instance = SwiftSimpleProgressIndicatorPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
+    SVProgressHUD.setDefaultMaskType(.black)
+
+    SVProgressHUD.setForegroundColor(UIColor(red: 39/255.0, green: 128/255.0, blue: 250/255.0, alpha: 1.0))
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+  if(call.method == "showProgressIndicator"){
+  let status:Bool = call.arguments as! Bool
+  if status == true {
+  SVProgressHUD.show()
+  }
+  else{
+  SVProgressHUD.dismiss()
+  }
+
+  }
+
   }
 }
